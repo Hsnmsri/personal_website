7a09291d9426678a54d711af210178c741b58434
Hossein Mansouri <hsnmsri82@gmail.com>
2024-02-20 02:02:51 +0330

    set width and height updater

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
+  <div :style="'width: ' + windowWidth + 'px;height: ' + windowHeight + 'px'">
     <router-view></router-view>
+  </div>
 </template>
+<script>
+export default {
+  mounted() {
+    setInterval(() => {
+      this.windowWidth = window.innerWidth;
+      this.windowHeight = window.innerHeight;
+    }, 1);
+  },
+  data() {
+    return {
+      windowWidth: null,
+      windowHeight: null,
+    };
+  },
+};
+</script>

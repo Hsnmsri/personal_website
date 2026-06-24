1154e51a5c9fe399d0f56dc46c54665ca960146e
Hossein Mansouri <hsnmsri82@gmail.com>
2026-06-24 14:39:48 +0330

    feat: create default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full h-full">
+
+        <!-- NAVBAR -->
+        <header class="fixed top-0 left-0 right-0 w-full">
+            <Navbar />
+        </header>
+        <!-- NAVBAR -->
+
+        <main class="pt-16.25">
+            <NuxtPage />
+        </main>
+
+    </div>
+</template>

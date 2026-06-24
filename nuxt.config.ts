2c9088dbcf76a734f584a7a4db3c159ee32a8d32
Hossein Mansouri <hsnmsri82@gmail.com>
2026-06-24 12:19:31 +0330

    feat: configure nuxt project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: {
+    enabled: true
+  },
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+
+  // VITE CONFIG
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
+
+  // STYLES
+  css: [
+    './app/assets/styles/tailwind.css',
+    './app/assets/styles/main.scss'
+  ]
 })

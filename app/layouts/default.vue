0e431b048c52bf914ce1aade12829c5ec7bd50c5
Hossein Mansouri <hsnmsri82@gmail.com>
2026-06-25 13:11:18 +0330

    feat: set z-50 for navbar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
     <div class="w-full h-full">
 
         <!-- NAVBAR -->
-        <header class="fixed top-0 left-0 right-0 w-full">
+        <header class="fixed z-50 top-0 left-0 right-0 w-full">
             <Navbar />
         </header>
         <!-- NAVBAR -->

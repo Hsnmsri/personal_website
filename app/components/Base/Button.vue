5bb68dd8bc5edf59723be852b233e379a57b3406
Hossein Mansouri <hsnmsri82@gmail.com>
2026-06-29 02:03:09 +0330

    feat: secondary variant added to button

--- app/components/Base/Button.vue
+++ app/components/Base/Button.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // MODEL
 interface Button {
-    variant?: 'white' | 'white-outline'
+    variant?: 'white' | 'white-outline' | 'secondary' | 'secondary-outline'
 }
 
 const props = defineProps<Button>();
@@ -11,6 +11,8 @@ const props = defineProps<Button>();
     <button class="px-5 py-2 rounded-full font-bold" :class="{
         'bg-white text-dark hover:bg-gray-300': props.variant === 'white',
         'bg-transparent border border-white text-white hover:bg-gray-300 hover:text-dark': props.variant === 'white-outline',
+        'bg-secondary text-dark hover:bg-secondary': props.variant === 'secondary',
+        'bg-transparent border border-secondary text-secondary hover:bg-secondary hover:text-dark': props.variant === 'secondary-outline',
     }">
         <slot />
     </button>
